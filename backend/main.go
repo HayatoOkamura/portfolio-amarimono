61a88fdc6f601360eadc8956013907c528530e75
makidaisuke851 <d.maki@fmc-inc.jp>
2025-04-24 15:39:19 +0900

    fix: render環境をCORSの設定に追加

--- backend/main.go
+++ backend/main.go
@@ -29,10 +29,11 @@ func main() {
 
 	// CORS設定を追加
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://portfolio-amarimono.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
-		AllowHeaders:     []string{"*"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowCredentials: true,
+		MaxAge:           12 * 60 * 60, // 12 hours
 	}))
 
 	// 静的ファイルを提供
