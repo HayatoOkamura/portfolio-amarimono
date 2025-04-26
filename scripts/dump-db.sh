aeb7ce224f6a3116e5b6471dfb7fc82677d712eb
HayatoOkamura <keepsmile.lucky7@gmail.com>
2025-04-26 20:41:49 +0900

    fix: github actions修正

--- scripts/dump-db.sh
+++ scripts/dump-db.sh
@@ -6,7 +6,10 @@ PGPASSWORD=postgres pg_dump -U postgres -h localhost -d postgres > dump.sql
 # 変更があった場合のみコミット
 if git diff --quiet dump.sql; then
   echo "No changes in database"
+  # 変更がない場合はdump.sqlを削除
+  rm dump.sql
 else
+  echo "Database changes detected"
   git add dump.sql
   git commit -m "Update database dump"
   git push
